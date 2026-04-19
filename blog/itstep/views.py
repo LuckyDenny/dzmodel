21d6420deb04c64bdd914481dc6763c781d61be4
Denis <bganka123@gmail.com>
2026-04-19 16:07:04 +0300

    dz

--- blog/itstep/views.py
+++ blog/itstep/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Course
+
+def get_course():
+    all = Course.objects.all()
+    count = all.count()
+    return {'cat1': all[:count // 2], 'cat2': all[count // 2:]}
 
 # Create your views here.
+def index(request):
+    courses = Course.objects.all().order_by('-start')
+    context = {'courses': courses}
+    context.update(get_course())
+    return render(request, "itstep/index.html", context)
