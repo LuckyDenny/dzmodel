14aeea431891de986f064c720a4a66d8d0c55e77
Denis <bganka123@gmail.com>
2026-05-08 17:23:29 +0300

    dz order

--- blog/order/models.py
+++ blog/order/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from shop.models import Product
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Order(models.Model):
@@ -12,6 +13,7 @@ class Order(models.Model):
     id = models.AutoField(primary_key=True)
     paid_amount = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"Order {self.id}"
--- blog/order/views.py
+++ blog/order/views.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .forms import OrderForm
 from .models import OrderItem
 from cart.cart import Cart
 
 # Create your views here.
+@login_required
 def create_order(request):
     cart = Cart(request)
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order= order, product= item['product'], quantity= item['quantity'], price= item['price'])
